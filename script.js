d4993bfe286a166aaf98b94e5dfd0b7ba5263c51
Efty <efty131331@gmail.com>
2021-12-03 22:37:33 -0800

    about section

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//       Navbar
+
 const menu = document.querySelector(".menu");
 const navOpen = document.querySelector(".hamburger");
 const navClose = document.querySelector(".close");
@@ -21,7 +23,14 @@ navClose.addEventListener("click", () => {
 
 //         Typed.js
 
-const typed = new Typed('#type1', {
+new Typed('#type1', {
+
+  strings: ['web developer', 'programmer','coder'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    loop: true,
+});
+new Typed('#type2', {
 
   strings: ['web developer', 'programmer','coder'],
     typeSpeed: 80,
@@ -46,3 +55,14 @@ const body = document.querySelector("body");
         body.classList.toggle('dark');
         dayNight.classList.toggle('active');
   }
+
+//          GSAP
+
+gsap.from(".logo", {opacity: 0, duration: 1, delay: 5, y: 10});
+gsap.from(".hamburger", {opacity: 0, duration: 1, delay: 1, x: 20});
+gsap.from(".banner", {opacity: 0, duration: 1, delay: 1.5, x: -200});
+gsap.from(".hero h3", {opacity: 0, duration: 1, delay: 2, y: 50});
+gsap.from(".hero h1", {opacity: 0, duration: 1, delay: 2.5, y: -45});
+gsap.from(".hero h4", {opacity: 0, duration: 1, delay: 3, y: -30});
+gsap.from(".hero a", {opacity: 0, duration: 1, delay: 3.5, x: -20});
+gsap.from(".nav-item", {opacity: 0, duration: 1, delay: 1.2, y: 30, stagger: 0.2,});
