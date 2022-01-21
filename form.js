0ca991dbcbd5de17655c336a0a267c7dab1faecf
Efty <efty131331@gmail.com>
2022-01-21 12:33:36 +0600

    working form and submit button

---
+++ form.js
@@ -0,0 +1,55 @@
+window.addEventListener("DOMContentLoaded", function () {
+
+    //select elements
+
+    var form = document.getElementById("my-form");
+    var status = document.getElementById("status");
+    //style p tag
+
+    status.style.color = "yellow";
+
+    // Success and Error functions for after the form is submitted
+  
+    function success() {
+      form.reset();
+      status.classList.add("success");
+      status.innerHTML = "Thanks for your submission!";
+      setTimeout(function(){
+        status.style.display = "none";
+        }, 3000);
+    }
+  
+    function error() {
+      status.classList.add("error");
+      status.innerHTML = "Oops! There was a problem.";
+    }
+
+  
+    // handle the form submission event
+  
+    form.addEventListener("submit", function (ev) {
+      ev.preventDefault();
+      var data = new FormData(form);
+      ajax(form.method, form.action, data, success, error);
+    });
+  });
+  
+  // helper function for sending an AJAX request
+  
+  function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        success(xhr.response, xhr.responseType);
+      } else {
+        error(xhr.status, xhr.response, xhr.responseType);
+      }
+    };
+    xhr.send(data);
+  }
+
+  // style the p tag
+  status
